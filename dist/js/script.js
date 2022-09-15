0a93dbceeb2282ae4657151bc9a2ebb9844538f2
roman <adnicepvp@gmail.com>
2022-09-15 13:47:49 +0300

    add tabs

--- dist/js/script.js
+++ dist/js/script.js
@@ -93,7 +93,43 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+
+  hideTabContent();
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+
+  showTabContent();
+  tabsParent.addEventListener('click', e => {
+    const target = e.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target === item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
 
 /***/ })
 
