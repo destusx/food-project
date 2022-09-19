f0866c8158937104c0b29e9026a83c6326a7dfff
roman <adnicepvp@gmail.com>
2022-09-19 14:22:09 +0300

    add fetch

--- src/js/main.js
+++ src/js/main.js
@@ -252,31 +252,29 @@ document.addEventListener('DOMContentLoaded', () => {
             `
             form.insertAdjacentElement('afterend', statusMessage)
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
 
-            request.setRequestHeader('Content-type', 'application/json')
             const formData = new FormData(form);
-            console.log(formData);
+        
             const object = {};
             formData.forEach(function(value, key) {
                 object[key] = value;
             });
-            console.log(object);
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response)
-                    showThanksModal(message.succses);
-                    statusMessage.remove()
-                    form.reset();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            }).then(data => data.text())
+            .then(data => {
+                console.log(data)
+                showThanksModal(message.succses);
+                statusMessage.remove()
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset()
             })
         })
     }
