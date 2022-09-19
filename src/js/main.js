a8e1097a2b29c43400985292d54004644a054045
roman <adnicepvp@gmail.com>
2022-09-19 16:28:16 +0300

    add json server

--- src/js/main.js
+++ src/js/main.js
@@ -302,4 +302,8 @@ document.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000)
     }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res))
 })
