4a24e374dbc5946adf59ee74fb0a6ebbacfdba5b
roman <adnicepvp@gmail.com>
2022-09-21 00:44:30 +0300

    add axios

--- dist/js/script.js
+++ dist/js/script.js
@@ -281,10 +281,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     return await res.json();
-  };
+  }; // getResource('http://localhost:3000/menu')
+  //     .then(data => {
+  //         data.forEach(({img, altimg, title, descr, price}) => {
+  //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+  //         });
+  //     });
 
-  getResource('http://localhost:3000/menu').then(data => {
-    data.forEach(({
+
+  axios.get('http://localhost:3000/menu').then(data => {
+    data.data.forEach(({
       img,
       altimg,
       title,
@@ -360,8 +366,6 @@ document.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }, 4000);
   }
-
-  fetch('http://localhost:3000/menu').then(data => data.json()).then(res => console.log(res));
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -205,13 +205,21 @@ document.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
-    getResource('http://localhost:3000/menu')
-        .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
-                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-            });
-        });
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //         });
+    //     });
 
+    axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
+            new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+        });
+    });
+    
+    
     //Forms
 
     const forms = document.querySelectorAll('form');
@@ -292,7 +300,4 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 4000)
     }
 
-    fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res))
 })
